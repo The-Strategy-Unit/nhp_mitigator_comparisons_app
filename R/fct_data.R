a69966faa66d6b67af3640032932e32dc20b539d
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-08-09 18:27:11 +0100

    Add function for demo dataset

---
+++ R/fct_data.R
@@ -0,0 +1,53 @@
+#' Generate a Simple Demo Dataset
+#' @param n_schemes Integer.
+#' @param n_mitigators Integer.
+#' @param seed Integer.
+#' @return A data.frame.
+#' @noRd
+generate_test_dataset <- function(
+    n_schemes = 10,
+    n_mitigators = 50,
+    n_mitigator_groups = 8,
+    seed = 123
+) {
+
+  scheme <- paste(
+    "Scheme",
+    stringr::str_pad(1:n_schemes, width = 2, side = "left", pad = "0")
+  )
+
+  mitigator <- paste(
+    "Mitigator",
+    stringr::str_pad(1:n_mitigators, width = 2, side = "left", pad = "0")
+  )
+
+  mitigator_lookup <- mitigator |>
+    split(
+      cut(
+        seq_along(mitigator),
+        n_mitigator_groups,
+        labels = paste("Group", 1:n_mitigator_groups)
+      )
+    ) |>
+    tibble::enframe(name = "mitigator_group", value = "mitigator") |>
+    tidyr::unnest(mitigator)
+
+  combos <- tidyr::crossing(scheme, mitigator) |>
+    dplyr::left_join(mitigator_lookup, by = "mitigator")
+
+  nrows <- nrow(combos)
+
+  withr::with_seed(
+    seed = seed,
+    code = {
+      combos |>
+        dplyr::mutate(
+          lo = runif(nrows),
+          hi = pmin(lo + runif(nrows, 0.25, 0.5), 1),
+          mid = (lo + hi) / 2,
+          years = sample(20:30, nrows, replace = TRUE)
+        )
+    }
+  )
+
+}
