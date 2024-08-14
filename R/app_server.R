008f06df172ea605d386b80780915d22e642dfc2
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-08-14 14:10:37 +0100

    Read from Azure, allow dev demo data, tweak plots

--- R/app_server.R
+++ R/app_server.R
@@ -6,14 +6,100 @@
 #' @noRd
 app_server <- function(input, output, session) {
 
-  # Reactives
+  # Reactives (Azure) ----
 
-  dat <- shiny::reactive({
-    generate_test_dataset()
+  if (getOption("golem.app.prod")) {  # avoids Azure calls in dev
+
+    container <- reactive({
+      get_container()  # relies on .Renviron variables
+    })
+
+    runs_meta <- reactive({
+      fetch_labelled_runs_meta(container())  # exposes run_stage metadata
+    })
+
+    params <- reactive({
+      fetch_labelled_runs_params(runs_meta(), container())  # final, intermediate, initial
+    })
+
+    extracted_params <- reactive({
+      extract_params(params(), runs_meta())
+    })
+
+  }
+
+  # Reactives (data read) ----
+
+  all_params <- reactive({
+    app_sys("app", "data", "all_params.json") |> jsonlite::fromJSON()
   })
 
-  peers <- shiny::reactive({
-    generate_peer_set(dat())
+  mitigator_lookup <- reactive({
+    app_sys("app", "data", "mitigator_name_lookup.csv") |>
+      read_mitigator_lookup()
+  })
+
+  mitigator_groups <- reactive({
+    app_sys("app", "data", "mitigator groupings.xlsx") |>
+      read_mitigator_groups()
+  })
+
+  nee <- reactive({
+    app_sys("app", "data", "nee_table.rds") |> read_nee()
+  })
+
+  trust_code_lookup <- reactive({
+    app_sys("app", "data", "NHP_trust_code_lookup.xlsx") |> readxl::read_excel()
+  })
+
+
+  # Reactives (data prep) ----
+
+  if (getOption("golem.app.prod")) {  # avoids Azure calls in dev
+
+    skeleton_table <- reactive({
+      extracted_params() |> prepare_skeleton_table()
+    })
+
+    dat <- reactive({
+      # TODO: probably update populate_table() with the extra wrangling below?
+      populate_table(
+        skeleton_table(),
+        extracted_params(),
+        mitigator_lookup(),
+        mitigator_groups(),
+        nee()
+      ) |>
+        dplyr::left_join(
+          trust_code_lookup() |>
+            dplyr::mutate(
+              scheme = `Trust ODS Code`,
+              scheme_name = paste0(`Name of Hospital site`, " (", scheme, ")"),
+              .keep = "none"
+            ),
+          by = "scheme"
+        ) |>
+        dplyr::rename(
+          mitigator = strategy,
+          mitigator_group = Grouping,
+          lo = value_1,
+          hi = value_2,
+          mid = midpoint,
+        ) |>
+        dplyr::mutate(years = horizon_year - baseline_year)
+    })
+
+  } else if (!getOption("golem.app.prod")) {  # read demo data in dev
+    dat <- reactive({
+      app_sys("app", "data", "demo-dat.csv") |>
+        readr::read_csv(show_col_types = FALSE)
+    })
+  }
+
+  peers <- reactive({
+    app_sys("app", "data", "peers.rds") |>  # from 'Trust Peer Finder Tool' online
+      readr::read_rds() |>
+      dplyr::rename(scheme = procode)
   })
 
   peer_set <- shiny::reactive({
@@ -25,9 +111,9 @@ app_server <- function(input, output, session) {
   all_schemes <- shiny::reactive({
     dat() |>
       shiny::req() |>
-      dplyr::distinct(scheme) |>
-      dplyr::pull() |>
-      sort()
+      dplyr::distinct(scheme_name, scheme) |>
+      dplyr::arrange(scheme_name) |>
+      tibble::deframe()  # named vector: value is code, name is scheme name
   })
 
   all_mitigators <- shiny::reactive({
@@ -45,6 +131,8 @@ app_server <- function(input, output, session) {
       dplyr::pull() |>
       sort()
   })
+
+  # Reactives (data selection) ----
 
   dat_selected_pointrange <- shiny::reactive({
 
@@ -110,10 +198,9 @@ app_server <- function(input, output, session) {
         mitigator %in% input$mitigators,
       )
 
-
   })
 
-  # Observers
+  # Observers ----
 
   shiny::observe({
     shiny::updateSelectInput(
@@ -189,7 +276,7 @@ app_server <- function(input, output, session) {
       pointrange <- pointrange +
         ggplot2::geom_pointrange(
           ggplot2::aes(
-            x = mid, y = scheme,
+            x = mid, y = scheme_name,
             xmin = lo, xmax = hi,
             colour = point_colour
           )
@@ -209,11 +296,15 @@ app_server <- function(input, output, session) {
         ggplot2::facet_wrap(~scheme, nrow = input$facet_rows)
     }
 
+    if (!input$toggle_horizon_pointrange) {
+      pointrange <- pointrange + ggplot2::xlim(0, 1)  # should be fixed if raw
+    }
+
     pointrange +
       ggplot2::scale_color_manual(
         values = c("FALSE" = "black", "TRUE" = "red")
       ) +
-      ggplot2::labs(x = "80% Confidence Interval") +
+      ggplot2::labs(x = "80% Prediction Interval") +
       ggplot2::theme_bw(base_size = 20) +
       ggplot2::theme(
         axis.title.y = ggplot2::element_blank(),
@@ -227,13 +318,16 @@ app_server <- function(input, output, session) {
     heatmap <-  dat_selected_heatmap() |>
       ggplot2::ggplot(
         ggplot2::aes(
-          x = mitigator,
-          y = scheme,
+          x = scheme_name,
+          y = mitigator,
           fill = value
         )
       ) +
       ggplot2::geom_tile() +
-      ggplot2::scale_x_discrete(position = "top") +
+      ggplot2::scale_x_discrete(
+        position = "top",
+        labels = \(x) stringr::str_wrap(x, width = 10)
+      ) +
       ggplot2::theme_minimal(base_size = 20) +
       ggplot2::theme(
         axis.title.x = ggplot2::element_blank(),
