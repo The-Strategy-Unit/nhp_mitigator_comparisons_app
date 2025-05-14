fdcb0f08e9036fd3db259407c25f71ef8d6651df
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-05-14 14:21:54 +0100

    Add double-deploy script

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -1,59 +1,24 @@
-# Building a Prod-Ready, Robust Shiny Application.
-#
-# README: each step of the dev files is optional, and you don't have to
-# fill every dev scripts before getting started.
-# 01_start.R should be filled at start.
-# 02_dev.R should be used to keep track of your development during the project.
-# 03_deploy.R should be used once you need to deploy your app.
-#
-#
-######################################
-#### CURRENT FILE: DEPLOY SCRIPT #####
-######################################
+# Deploy to the current and new Posit Connect servers. The current server
+# (strategyunitwm.nhs.uk) will be switched off in May/June 2025 and the new
+# server (currently named su.mlcsu.org) will take its name.
 
-# Test your app
+deploy <- function(server_name, app_id) {
+  rsconnect::deployApp(
+    appName = "nhp_inputs_report_app",
+    appTitle = "NHP Mitigator Comparison App",
+    appFiles = c(
+      "R/",
+      "inst/",
+      "NAMESPACE",
+      "DESCRIPTION",
+      "app.R"
+    ),
+    server = server_name,
+    appId = app_id,
+    lint = FALSE,
+    forceUpdate = TRUE
+  )
+}
 
-## Run checks ----
-## Check the package before sending to prod
-# devtools::check()
-# rhub::check_for_cran()
-
-# Deploy
-
-## Local, CRAN or Package Manager ----
-## This will build a tar.gz that can be installed locally,
-## sent to CRAN, or to a package manager
-# devtools::build()
-
-## RStudio ----
-## If you want to deploy on RStudio related platforms
-# golem::add_rstudioconnect_file()
-# golem::add_shinyappsio_file()
-# golem::add_shinyserver_file()
-
-## Docker ----
-## If you want to deploy via a generic Dockerfile
-# golem::add_dockerfile_with_renv()
-
-## If you want to deploy to ShinyProxy
-# golem::add_dockerfile_with_renv_shinyproxy()
-
-
-# Deploy to Posit Connect or ShinyApps.io
-# In command line.
-rsconnect::deployApp(
-  appName = "nhp_inputs_report_app",
-  appTitle = "NHP Mitigator Comparison App",
-  appFiles = c(
-    # Add any additional files unique to your app here.
-    "R/",
-    "inst/",
-    # "data/",
-    "NAMESPACE",
-    "DESCRIPTION",
-    "app.R"
-  ),
-  appId = rsconnect::deployments(".")$appID,
-  lint = FALSE,
-  forceUpdate = TRUE
-)
+deploy("connect.strategyunitwm.nhs.uk", 298)
+deploy("connect.su.mlcsu.org", 108)
